a127027e9b8f0af75b1cceab7200c34e3958cad2
Fernando Cargnelutti <fgcargnelutti@gmail.com>
2026-06-02 17:14:48 +0100

    big UI refactor

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './styles/design-tokens.css'
+import './styles/game-motion.css'
+import './styles/game-background.css'
 import './styles/ornate-ui.css'
+import './styles/game-surfaces.css'
+import './styles/game-buttons.css'
 import './styles/square-ui.css'
 import App from './App.tsx'
 
